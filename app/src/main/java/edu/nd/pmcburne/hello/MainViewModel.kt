c30377c725a8e09a31cb6174434b22630fc3858f
Will-McBurney <pmcburn1@gmail.com>
2026-01-28 12:00:07 -0500

    added some comment documentation

--- app/src/main/java/edu/nd/pmcburne/hello/MainViewModel.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/MainViewModel.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+/** the state of the counter on our main screen */
 data class MainUIState(
     val counterValue: Int
 )
@@ -16,26 +17,32 @@ class MainViewModel(
     private val _uiState = MutableStateFlow(MainUIState(initialCounterValue))
     val uiState: StateFlow<MainUIState> = _uiState.asStateFlow()
 
+    /** increments the counter by 1 */
     fun incrementCounter() {
         _uiState.update{ currentState ->
             currentState.copy(counterValue = _uiState.value.counterValue + 1)
         }
     }
 
+    /** decrements the counter by 1 */
     fun decrementCounter() {
         _uiState.update{ currentState ->
             currentState.copy(counterValue = _uiState.value.counterValue - 1)
         }
     }
 
+    /** resets the counter to 0 */
     fun resetCounter() {
         _uiState.update { currentState ->
             currentState.copy(counterValue = 0)
         }
     }
 
+    /** checks if the increment button should be enabled */
     val isDecrementEnabled: Boolean
         get() = _uiState.value.counterValue > 0
+
+    /** checks if the decrement button should be enabled */
     val isResetEnabled: Boolean
         get() = _uiState.value.counterValue > 0
 }
