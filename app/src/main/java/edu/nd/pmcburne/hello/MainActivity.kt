a88e10795bf0dcca49e7d8f2611288744b94f7cd
Will-McBurney <pmcburn1@gmail.com>
2026-01-28 11:17:57 -0500

    Initial "Working" counter using rememberSaveable

--- app/src/main/java/edu/nd/pmcburne/hello/MainActivity.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/MainActivity.kt
@@ -1,16 +1,32 @@
 package edu.nd.pmcburne.hello
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.nd.pmcburne.hello.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +44,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            "Welcome to the Counter App!"
+        )
+        Spacer(modifier = modifier.height(16.dp))
+        Counter(initialValue = 0)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+@Preview(showBackground = true)
+fun PreviewMainScreen() {
     MyApplicationTheme {
-        Greeting("Android")
+        MainScreen()
+    }
+}
+
+@Composable
+fun Counter(initialValue: Int = 0, modifier: Modifier = Modifier) {
+    var value by rememberSaveable { mutableStateOf(initialValue) }
+    Row {
+        Text("Value: $value")
+        Button(
+            onClick = { value++ },
+            modifier = modifier
+        ) {
+            Text("+")
+        }
+        Button(
+            onClick = { value-- },
+            enabled = value > 0,
+            modifier = modifier
+        ) {
+            Text("-")
+        }
+        Button(
+            onClick = { value = 0 },
+            enabled = value > 0,
+            modifier = modifier
+        ) {
+            Text("Reset")
+        }
+
+    }
+}
+
+
+@Preview(name = "Light Mode Counter", showBackground = true)
+@Preview(name = "Dark Mode Counter", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun CounterPreview() {
+    MyApplicationTheme {
+        Counter(0)
     }
 }
--- app/src/main/java/edu/nd/pmcburne/hello/ui/theme/Theme.kt
+++ app/src/main/java/edu/nd/pmcburne/hello/ui/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -35,7 +36,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MyApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
